019a512c62d3a13a8b5aa6c2bf00ebac4a5c6d41
t.i <isoittech@gmail.com>
2021-07-07 08:09:38 +0900

    エンコーディング問題を解決した；タスク起動間隔を引数で指定可能にした

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ RPAにより定期的にマウスカーソルを移動します
 Windows端末においてスクリーンロック時間を管理できない（権限により変更できない）状況下で、
 本ツールによりマウスがグリグリ動くことで、スクリーンロックされなくなります。
 
+## how to use
+
+1. build/libs/AlibiKun-x.x.x-yyyy.jarをダブルクリック
+
+1. java -jar ${JAR PATH}/AlibiKun-x.x.x-yyyy.jar  
+※1分ごとに起動
+
+1. java -jar ${JAR PATH}/AlibiKun-x.x.x-yyyy.jar ＜INTEGER＞   
+※指定した時間（分）ごとに起動
+
 ## how to build
 
 ./gradlew shadowJar
--- src/main/java/jp/isoittech/tools/alibikun/AlibiKunApplication.java
+++ src/main/java/jp/isoittech/tools/alibikun/AlibiKunApplication.java
@@ -1,16 +1,25 @@
 package jp.isoittech.tools.alibikun;
 
+import java.text.MessageFormat;
+
 import it.sauronsoftware.cron4j.Scheduler;
 import jp.isoittech.tools.alibikun.service.MouseMove;
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class AlibiKunApplication {
 
-	public static void main(String[] args) {
-		log.info("メイン処理開始");
+    public static void main(String[] args) {
+
+        String moveInterval = "1";
+        if (args.length >= 1) {
+            moveInterval = args[0];
+        }
+
+        log.info(MessageFormat.format("メイン処理開始: {0}分毎に動作します。",moveInterval));
         Scheduler scheduler = new Scheduler();
-        scheduler.schedule("* * * * *", new MouseMove());
+        scheduler.schedule(MessageFormat.format("*/{0} * * * *", moveInterval), new MouseMove());
         scheduler.start();
-	}
+    }
 
 }
